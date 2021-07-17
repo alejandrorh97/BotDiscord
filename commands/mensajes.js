4bda6f029daaea6154708a134a21151d4c0d201d
OC18018 <OC18018@ues.edu.sv>
2021-07-16 19:09:21 -0600

    "mensajes.js"

---
+++ commands/mensajes.js
@@ -0,0 +1,10 @@
+const {Client,MessageEmbed, TextChannel} = require('discord.js');
+
+module.exports = {
+    nombre: "msj",
+    descripcion: "Tendria que mandar un msj a otro canal :v joer",
+
+ejecutar (cliente,message,args){
+    TextChannel.name(pruebas)
+ },
+};
