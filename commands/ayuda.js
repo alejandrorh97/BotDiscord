96ccbb9c539c8f777414a45e7892845ff56a3493
Alejandro Rivas <alejandrorrh97@gmail.com>
2021-08-02 17:25:21 -0600

    Ahora el comando ayuda muestra los diferentes comandos dependiendo si es admin o mortal

--- commands/ayuda.js
+++ commands/ayuda.js
@@ -1,38 +1,54 @@
 const { prefix } = require("../config.json");
-const {MessageEmbed} = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-	nombre: "ayuda",
-	descripcion: "Lista todos los comando disponibles.",
-	args: false,
-	soloServer: false,
-	usos: "[comando]",
-	ejecutar(cliente,mensaje, args) {
-		let datos = [];
-		let { commands } = mensaje.client;
-		let embebido = new MessageEmbed();
-		embebido.setColor("RED");
-		if (!args.length) {
-			embebido.setTitle("Comandos disponibles");
-			for (let comando of commands){
-				embebido.addField(`${prefix}${comando[1].nombre}`,comando[1].descripcion);
-			}
-			embebido.addField("Mas informacion",`Utiliza ${prefix}ayuda [comando] para obtener mas informacion sobre ese comando`);
-		}
-		else if(args.length >= 2){
-			embebido.addField("Error","Muchos parametros solo 1");
+    nombre: "ayuda",
+    descripcion: "Lista todos los comando disponibles.",
+    args: false,
+    admins: false,
+    borrable: true,
+    soloServer: false,
+    usos: "[comando]",
+    ejecutar(cliente, mensaje, args) {
+        let datos = [];
+        let { commands } = mensaje.client;
+        let embebido = new MessageEmbed();
+        if (!args.length) {
+            embebido.setTitle("Comandos disponibles");
+            for (let comando of commands) {
+                //ver si solo admins
+                if (comando[1].admins && !mensaje.member.hasPermission('ADMINISTRATOR')) {
+                    continue;
+                }
+
+                embebido.addField(
+                    `${prefix}${comando[1].nombre}`,
+                    comando[1].descripcion
+                );
+            }
+            embebido.addField(
+                "Mas informacion",
+                `Utiliza ${prefix}ayuda [comando] para obtener mas informacion sobre ese comando`
+            );
+        } else if (args.length >= 2) {
+            embebido.addField("Error", "Muchos parametros solo 1");
+        } else {
+            if (commands.has(args[0])) {
+                comando = commands.get(args[0]);
+                embebido.setTitle(`Comando ${args[0]}`);
+                embebido.addField("Descripcion", comando.descripcion);
+                embebido.addField("Uso", `${prefix}${args[0]} ${comando.usos}`);
+            } else {
+                embebido.addField("No existe el comando", "");
+            }
+        }
+		if(mensaje.member.hasPermission('ADMINISTRATOR')){
+			embebido.setColor("RED");
 		}
 		else {
-			if(commands.has(args[0])){
-				comando = commands.get(args[0])
-				embebido.setTitle(`Comando ${args[0]}`);
-				embebido.addField("Descripcion",comando.descripcion);
-				embebido.addField("Uso",`${prefix}${args[0]} ${comando.usos}`);
-			}
-			else{
-				embebido.addField("No existe el comando","");
-			}
+			embebido.setColor("BLUE");
 		}
-		mensaje.channel.send(embebido);
-	}
+		
+        mensaje.channel.send(embebido);
+    },
 };
